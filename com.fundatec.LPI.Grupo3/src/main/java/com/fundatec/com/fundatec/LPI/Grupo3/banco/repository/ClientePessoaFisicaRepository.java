dff8d05b01618fe50f9d6033920763d4823a39a2
Matheus <matheusbj95@gmail.com>
2022-07-06 14:29:47 -0300

    Criado classes ClientePessoaFisicaController, ClientePessoaFisicaRepository, ClientePessoaFisicaService, ClientePessoaJuridicaRepository, ClientePessoaJuridicaService e deletado as Classes Cliente.java, ClienteController.java, ClienteRepository.java e ClienteService.java

---
+++ com.fundatec.LPI.Grupo3/src/main/java/com/fundatec/com/fundatec/LPI/Grupo3/banco/repository/ClientePessoaFisicaRepository.java
@@ -0,0 +1,11 @@
+package com.fundatec.com.fundatec.LPI.Grupo3.banco.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientePessoaFisicaRepository extends CrudRepository {
+
+
+
+}
