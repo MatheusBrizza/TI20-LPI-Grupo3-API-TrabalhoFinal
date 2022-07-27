bbde51399a90d3fc2d6285e674b42dc04985db62
melissabgoulart@gmail.com <melissabgoulart@example.com>
2022-07-27 19:25:38 -0300

    Entrega final

--- com.fundatec.LPI.Grupo3/src/main/java/com/fundatec/com/fundatec/LPI/Grupo3/servicoPessoas/DTO/PessoaJuridicaDTO.java
+++ com.fundatec.LPI.Grupo3/src/main/java/com/fundatec/com/fundatec/LPI/Grupo3/servicoPessoas/DTO/PessoaJuridicaDTO.java
@@ -1,11 +1,4 @@
 package com.fundatec.com.fundatec.LPI.Grupo3.servicoPessoas.DTO;
-package com.
-package com.fundatec.LPI.Grupo3/src/main/java/com/fundatec/com/fundatec/LPI/Grupo3/banco/repository/servicoPessoas/DTO/PessoaJuridicaDTO.java
-package com.fundatec.com.fundatec.LPI.Grupo3.banco.repository.servicoPessoas.DTO;
-package com.fundatec.com.fundatec.LPI.Grupo3.servicoPessoas.DTO;
-
-
->>>>>>> ad3bde725c4ab75809d7bea62134f77b7989a998:com.fundatec.LPI.Grupo3/src/main/java/com/fundatec/com/fundatec/LPI/Grupo3/servicoPessoas/DTO/PessoaJuridicaDTO.java
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
